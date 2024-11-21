9bb7444c306fce3ed774c85ea89b9b139f190ea0
Your Name <avanomme@upei.ca>
2024-11-21 19:31:38 -0400

    Add maintenance mode check in maintenance.php

---
+++ web/maintenance.php
@@ -0,0 +1,21 @@
+<?php
+// Add at the top of the file
+if (!file_exists('../settings.php'))
+    exit('Not configured');
+
+require '../vendor/autoload.php';
+
+$app = new \RKA\Slim();
+$app->setMode('production');
+
+// Get app state
+$app->configService = \Xibo\Service\ConfigService::Load('../config/config.php');
+
+// Check if maintenance mode is actually enabled
+$maintenanceEnabled = $app->configService->getSetting('MAINTENANCE_ENABLED');
+
+if ($maintenanceEnabled !== 1 && $maintenanceEnabled !== '1' && $maintenanceEnabled !== 'On') {
+    // Redirect to index
+    header('Location: index.php');
+    exit();
+} 
